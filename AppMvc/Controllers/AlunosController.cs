dba2ae31ffdb26b05ae9e93e9b396dfb17d73153
Delsivan Nascimento Vieira <delsivannascimentovieira@gmail.com>
2022-02-27 20:18:29 -0300

    Customizando a implementação padrão (Rotas)

--- AppMvc/Controllers/AlunosController.cs
+++ AppMvc/Controllers/AlunosController.cs
@@ -13,39 +13,43 @@ namespace AppMvc.Controllers
 {
     public class AlunosController : Controller
     {
-        private ApplicationDbContext db = new ApplicationDbContext();
+        private readonly ApplicationDbContext db = new ApplicationDbContext();
 
         // GET: Alunos
+        [HttpGet]
+        [Route("listar-alunos")]
         public async Task<ActionResult> Index()
         {
             return View(await db.Alunos.ToListAsync());
         }
 
         // GET: Alunos/Details/5
-        public async Task<ActionResult> Details(int? id)
+        [HttpGet]
+        [Route("aluno-detalhe/{id:int}")]
+        public async Task<ActionResult> Details(int id)
         {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Aluno aluno = await db.Alunos.FindAsync(id);
+            
+            var aluno = await db.Alunos.FindAsync(id);
+
             if (aluno == null)
             {
                 return HttpNotFound();
             }
+
             return View(aluno);
         }
 
         // GET: Alunos/Create
+        [HttpGet]
+        [Route("novo-aluno")]
         public ActionResult Create()
         {
             return View();
         }
 
         // POST: Alunos/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
+        [Route("novo-aluno")]
         [ValidateAntiForgeryToken]
         public async Task<ActionResult> Create([Bind(Include = "Id,Nome,Email,CPF,DataMatricula,Ativo")] Aluno aluno)
         {
@@ -60,13 +64,13 @@ namespace AppMvc.Controllers
         }
 
         // GET: Alunos/Edit/5
-        public async Task<ActionResult> Edit(int? id)
+        [HttpGet]
+        [Route("editar-aluno/{id:int}")]
+        public async Task<ActionResult> Edit(int id)
         {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Aluno aluno = await db.Alunos.FindAsync(id);
+            
+            var aluno = await db.Alunos.FindAsync(id);
+
             if (aluno == null)
             {
                 return HttpNotFound();
@@ -75,9 +79,8 @@ namespace AppMvc.Controllers
         }
 
         // POST: Alunos/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
+        [Route("editar-aluno/{id:int}")]
         [ValidateAntiForgeryToken]
         public async Task<ActionResult> Edit([Bind(Include = "Id,Nome,Email,CPF,DataMatricula,Ativo")] Aluno aluno)
         {
@@ -91,22 +94,24 @@ namespace AppMvc.Controllers
         }
 
         // GET: Alunos/Delete/5
-        public async Task<ActionResult> Delete(int? id)
+        [HttpGet]
+        [Route("excluir-aluno/{id:int}")]
+        public async Task<ActionResult> Delete(int id)
         {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
+            
             Aluno aluno = await db.Alunos.FindAsync(id);
+
             if (aluno == null)
             {
                 return HttpNotFound();
             }
+
             return View(aluno);
         }
 
         // POST: Alunos/Delete/5
-        [HttpPost, ActionName("Delete")]
+        [HttpPost]
+        [Route("excluir-aluno/{id:int}")]
         [ValidateAntiForgeryToken]
         public async Task<ActionResult> DeleteConfirmed(int id)
         {
